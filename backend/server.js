a04067dca8847703892fb97a2dfa2c461da05b0e
Femi <phemorah@gmail.com>
2025-02-04 08:38:07 +0100

    Mongo database connection established

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+// const express = require("express");
 import express from "express";
+import dotenv from "dotenv"; 
+import { connectDB } from "./config/db.js";
+
+dotenv.config(); 
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+    res.send("Server is ready now");
+}) 
 
 app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000");
+    connectDB();
+    console.log("Server started at http://localhost:5000 here");
 });
+
+// 
